53c3f9e95043805881ef1373376a48534ed98a14
rizzabh <pandeyrishabh966@gmail.com>
2024-01-27 22:06:37 +0530

    Fix: Bottom bar state

--- nutrisnap-frontend/src/app/bodysnap/page.jsx
+++ nutrisnap-frontend/src/app/bodysnap/page.jsx
@@ -271,6 +271,59 @@ const ImageUploader = () => {
           </div>
         </div>
       </div>
+      <div className="bottom-navigation bottom-0 fixed w-full p-4 md:hidden bg-white shadow-2xl h-fit">
+        <div className="flex items-center justify-around md:hidden">
+          <div className="flex flex-col items-center">
+            <a href="/foodsnap">
+              <img
+                src="/food.png"
+                alt=""
+                height={30}
+                width={30}
+                className={` mx-auto opacity-40`}
+              />
+              <div className="text-xs text-center">Food</div>
+            </a>
+          </div>
+          <div className="flex flex-col items-center">
+            <a href="/skinsnap">
+              <img
+                src="/face.png"
+                alt=""
+                height={30}
+                width={30}
+                className={` mx-auto opacity-40 hover:opacity-100`}
+              />
+              <div className="text-xs text-center">Skin</div>
+            </a>
+          </div>
+          <div className="flex flex-col items-center">
+            <a href="/bodysnap">
+              <img
+                src="/body.png"
+                alt=""
+                height={30}
+                width={30}
+                className={` mx-auto opacity-100 active:opacity-100`}
+              />
+              <div className="text-xs text-center">Body</div>
+            </a>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <a href="/nutricon">
+              <img
+                src="/nutricon.png"
+                alt=""
+                height={30}
+                width={30}
+                className={` mx-auto opacity-40`}
+              />
+              <div className="text-xs text-center">Nutricon</div>
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
