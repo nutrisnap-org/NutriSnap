3f1a433e2a592bc718b4b528b2ad0cb80ff0f95a
Rahul <rahulsinghhh2312@gmail.com>
2024-02-13 22:22:24 +0530

    Update page.jsx

--- nutrisnap-frontend/src/app/skinsnap/page.jsx
+++ nutrisnap-frontend/src/app/skinsnap/page.jsx
@@ -159,7 +159,7 @@ const ImageUploader = () => {
   const fetchAnalysisData = async (file) => {
     try {
       setLoading(true);
-      const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GOOGLE_API_KEY);
+      const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GOOGLE_API_KEYI);
       const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
 
       const generationConfig = {
