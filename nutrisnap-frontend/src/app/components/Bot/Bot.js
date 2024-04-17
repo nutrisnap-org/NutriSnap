2ae706a93164252d463496d875ce8f3d93f81466
Rahul <rahulsinghhh2312@gmail.com>
2024-04-17 18:49:13 +0530

    bot w & h fix

--- nutrisnap-frontend/src/app/components/Bot/Bot.js
+++ nutrisnap-frontend/src/app/components/Bot/Bot.js
@@ -28,7 +28,9 @@ const Bot = () => {
       {isActive && (
         <>
           <iframe
-         
+            className="bottom-0 max-sm:w-11/12 max-sm:m-2 right-0 fixed m-8 rounded-md backdrop-blur-md shadow-xl border border-violet-600/30"
+            width={450}
+            height={560}
             src="https://nutrisnap-chat.vercel.app/"
           ></iframe>
         </>
