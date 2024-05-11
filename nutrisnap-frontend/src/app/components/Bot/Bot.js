caf7326bf76d3585a0ce89f35650d23a8631d0c1
Rahul <rahulsinghhh2312@gmail.com>
2024-05-12 02:56:15 +0530

    [bot fix ] in staging

--- nutrisnap-frontend/src/app/components/Bot/Bot.js
+++ nutrisnap-frontend/src/app/components/Bot/Bot.js
@@ -27,11 +27,12 @@ const Bot = () => {
       </div>
       {isActive && (
         <>
-          <a
+          <iframe
             className="bottom-0 max-sm:w-11/12 max-sm:m-2 right-0 fixed m-8 rounded-md backdrop-blur-md shadow-xl border border-violet-600/30"
-          
+            width={450}
+            height={560}
             src="https://nutrisnap-chat.vercel.app/"
-          ></a>
+          ></iframe>
         </>
       )}
     </div>
