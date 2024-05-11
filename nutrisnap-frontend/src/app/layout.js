cd20cafd2713ca2ca5f875bf5fdadd289cc8b084
Rahul <rahulsinghhh2312@gmail.com>
2024-05-12 01:39:13 +0530

    adding for iphone

--- nutrisnap-frontend/src/app/layout.js
+++ nutrisnap-frontend/src/app/layout.js
@@ -10,16 +10,45 @@ const inter = DM_Sans({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   const [isInstagramInAppBrowser, setIsInstagramInAppBrowser] = useState(false);
+  const [isIPhone, setIsIPhone] = useState(false);
 
   useEffect(() => {
-    // Check if navigator is available and user agent contains "Instagram"
-    if (typeof navigator !== "undefined" && navigator.userAgent.includes("Instagram")) {
-      setIsInstagramInAppBrowser(true);
+    // Check if navigator is available
+    if (typeof navigator !== "undefined") {
+      const ua = navigator.userAgent || navigator.vendor;
+      // Check if user agent contains "Instagram"
+      setIsInstagramInAppBrowser(ua.indexOf('Instagram') > -1);
+      // Check if user agent contains "iPhone"
+      setIsIPhone(/iPad|iPhone|iPod/.test(ua));
     }
   }, []);
+
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
+        {/* Render the "Open in browser" button if user is in Instagram in-app browser on Android */}
+        {!isIPhone && isInstagramInAppBrowser && (
+          <div className="mt-4 text-center">
+            <a href={location.href} target="_blank" download>
+              Open in browser (Android)
+            </a>
+          </div>
+        )}
+        {/* Render the special button for iPhone users on Instagram */}
+        {isIPhone && isInstagramInAppBrowser && (
+          <div className="mt-4 text-center">
+            <button onClick={() => {
+              // Handle the action for iPhone users on Instagram
+              if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+                window.location.href = 'googlechrome://nutrisnap.tech';
+              } else {
+                window.location.href = 'intent:https://nutrisnap.tech#Intent;end';
+              }
+            }}>
+              Special Button for iPhone Users on Instagram
+            </button>
+          </div>
+        )}
         {/* Render everything if user is not in Instagram in-app browser */}
         {!isInstagramInAppBrowser && (
           <>
@@ -29,14 +58,6 @@ export default function RootLayout({ children }) {
             <Bot />
             <Footer />
           </>
-        )}
-        {/* Render the "Open in browser" button if user is in Instagram in-app browser */}
-        {isInstagramInAppBrowser && (
-          <div className="mt-4 text-center">
-            <a href={location.href} target="_blank" download>
-              Open in browser
-            </a>
-          </div>
         )}
       </body>
     </html>
