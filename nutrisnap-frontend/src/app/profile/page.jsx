96c03838aa71fd6eea18d507e476fd149b6842a4
Rahul <rahulsinghhh2312@gmail.com>
2024-05-25 13:05:40 +0530

    [FEAT] Food Calender : depl fix

---
+++ nutrisnap-frontend/src/app/profile/page.jsx
@@ -0,0 +1,15 @@
+// components/Profile.js
+import React from 'react';
+
+const Profile = ({  }) => {
+  return (
+    <div>
+      <h1>hi</h1>
+      <p>hi</p>
+    </div>
+  );
+};
+
+
+
+export default Profile;
